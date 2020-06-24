8c159747c7676af6b4db440e5506b61e26905038
ferrazjoao <ferraz_joao@outlook.com>
2020-06-24 19:52:55 -0300

    iniciando primeira tela

--- projeto/App.js
+++ projeto/App.js
@@ -1,14 +1,25 @@
-import React , {useState} from 'react';
-import { View , Alert} from 'react-native';
+
+import React from 'react';
+import {SafeAreaView, ImageBackground, View, Text, Image} from 'react-native';
+import img from './src/img/background_1.jpg';
+import Botao from './src/components/Botao';
 import CaixaDeTexto from './src/components/CaixaDeTexto';
 
 
 export default function App() {
-    const [value, onChangeText] = useState('');
+  
 
   return (
-      <CaixaDeTexto placeholder= {'Email'} onChangeText={onChangeText} value={value}>
-          
-      </CaixaDeTexto>
+    <SafeAreaView style={{ flex: 1 }}>
+        <ImageBackground source = {img} style={{flex:1}}>
+        <View>
+            <Text style={{ color: 'white' }} >LOGIN
+            </Text>
+          </View>
+          <View>
+              <CaixaDeTexto placeholder ={'jaiminho@gmail.com'} onChangeText={onChangeText} value={value} />
+          </View>
+          </ImageBackground>
+      </SafeAreaView>
   )
 }
